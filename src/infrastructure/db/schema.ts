6d609731996443aec9332e013fbb6d125936ac7c
aboodtahetah18-commits <aboodtahetah18@gmail.com>
2026-09-17 19:18:37 +0300

    feat(conversations): export conversation schema

--- src/infrastructure/db/schema.ts
+++ src/infrastructure/db/schema.ts
@@ -2,3 +2,4 @@ export * from './auth-schema';
 export * from './financial-schema';
 export * from './algorithm-governance-schema';
 export * from './authorization-schema';
+export * from './conversation-schema';
