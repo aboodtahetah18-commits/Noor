7908a786ec9b51fc252b73018fbec843ff8ce944
aboodtahetah18-commits <aboodtahetah18@gmail.com>
2026-09-23 15:59:58 +0300

    Prioritize reviewed central governance documents

--- src/content/governance/current-overrides.ts
+++ src/content/governance/current-overrides.ts
@@ -1,10 +1,12 @@
 import { CENTRAL_ACTIVE_POLICIES } from './central-active-policies';
 import { CENTRAL_ACTIVE_REGULATIONS } from './central-active-regulations';
+import { REVIEWED_CENTRAL_DOCUMENTS } from './reviewed-central-20260923';
 import { REVIEWED_HILAL_DOCUMENTS } from './reviewed-hilal-20260923';
 import { REVIEWED_MALAA_DOCUMENTS } from './reviewed-malaa-20260923';
 import { REVIEWED_ASSETS_DOCUMENTS } from './reviewed-assets-20260923';
 
 export const CURRENT_GOVERNANCE_OVERRIDES=[
+  ...REVIEWED_CENTRAL_DOCUMENTS,
   ...REVIEWED_HILAL_DOCUMENTS,
   ...REVIEWED_MALAA_DOCUMENTS,
   ...REVIEWED_ASSETS_DOCUMENTS,
