17189e95b5e3142a59ad21605f580815ddda66b6
aboodtahetah18-commits <aboodtahetah18@gmail.com>
2026-09-23 15:58:07 +0300

    Prioritize reviewed bank governance documents

--- src/content/governance/current-overrides.ts
+++ src/content/governance/current-overrides.ts
@@ -1,7 +1,13 @@
 import { CENTRAL_ACTIVE_POLICIES } from './central-active-policies';
 import { CENTRAL_ACTIVE_REGULATIONS } from './central-active-regulations';
+import { REVIEWED_HILAL_DOCUMENTS } from './reviewed-hilal-20260923';
+import { REVIEWED_MALAA_DOCUMENTS } from './reviewed-malaa-20260923';
+import { REVIEWED_ASSETS_DOCUMENTS } from './reviewed-assets-20260923';
 
 export const CURRENT_GOVERNANCE_OVERRIDES=[
+  ...REVIEWED_HILAL_DOCUMENTS,
+  ...REVIEWED_MALAA_DOCUMENTS,
+  ...REVIEWED_ASSETS_DOCUMENTS,
   ...CENTRAL_ACTIVE_POLICIES,
   ...CENTRAL_ACTIVE_REGULATIONS,
   ...[
